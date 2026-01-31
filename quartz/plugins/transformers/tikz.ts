5080b9e0ce7eb554089c3471f801f303101e5821
Alex Kostin <kostin.ak@mail.ru>
2026-02-01 02:48:54 +0600

    Add TikZ support

--- quartz/plugins/transformers/tikz.ts
+++ quartz/plugins/transformers/tikz.ts
@@ -4,35 +4,15 @@ import { visit } from "unist-util-visit"
 export const Tikz: QuartzTransformerPlugin = () => {
   return {
     name: "Tikz",
-    html: (tree) => {
-      visit(tree, "element", (node: any, index, parent: any) => {
-        // Ищем стандартный блок кода: <pre><code class="language-tikz">
-        if (node.tagName === "pre" && node.children?.[0]?.tagName === "code") {
-          const codeNode = node.children[0]
-          const className = codeNode.properties?.className || []
-          
-          // Проверяем, есть ли класс language-tikz
-          if (className.some((c: string) => c === "language-tikz" || c === "tikz")) {
-             // Достаем исходный текст (он тут еще целый, одной строкой)
-             const text = codeNode.children[0]?.value || ""
-             
-             // Заменяем <pre>...</code> на <script type="text/tikz">...</script>
-             parent.children[index] = {
-               type: "element",
-               tagName: "script",
-               properties: {
-                 type: "text/tikz",
-                 // Можно добавить data-show-console="true" для отладки, если не рисует
-               },
-               children: [{
-                 type: "text",
-                 value: text
-               }]
-             }
-          }
+    markdown: (tree) => { // Обратите внимание: markdown, а не html
+      visit(tree, "code", (node: any) => {
+        // Проверяем язык блока ```tikz
+        if (node.lang === "tikz") {
+          // Превращаем блок кода в HTML-узел (прямая инъекция)
+          node.type = "html"
+          node.value = `<script type="text/tikz">\n${node.value}\n</script>`
         }
       })
-      return tree
     },
   }
 }
